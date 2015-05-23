0f9ae36c147a4708dd37fbfd0947177378f251e0
JJ Merelo <jjmerelo@gmail.com>
2015-05-23 12:17:32 +0200

    Procesados datos de MADE

---
+++ data/MADE/process.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+
+use File::Slurp::Tiny qw(read_lines);
+my @files = <d*.csv>;
+
+my @data = ['Gen','Id','Fitness'];
+for my $f (@files) {
+    my ($gen, $id) = ($f =~ /(\d+)_(\d+)/);
+    my @f_content = read_lines( $f );
+    
+    for my $l ( @f_content[1..$#f_content] ) {
+      chop $l;
+      my @values=split(/;/,$l);
+      $values[$#values] =~ s/,/./;
+      push @data, [$gen, $id, $values[$#values]];
+    }
+    
+}
+
+say join("\n",map join(",",@$_), @data);
+
