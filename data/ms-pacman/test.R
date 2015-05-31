96ee0feb89cd5e59c91736182c5be6b36dee5fa5
JJ Merelo <jjmerelo@gmail.com>
2015-05-31 18:40:06 +0200

    More kurtosis-skewness in another experiment

--- data/ms-pacman/test.R
+++ data/ms-pacman/test.R
@@ -1,24 +1,19 @@
 library(ggplot2)
 
 #Function definitions
-process.to.df <- function( data ){
-    df <- data.frame(ID=character(),
+process.to.df <- function( data, gen ){
+    df <- data.frame(Gen=character(),
+                     ID=character(),
                      Fitness=character())
     for (i in 1:length(data)) {
         for ( j in 1:length(data[,i])) {
-            df <- rbind( df, data.frame(ID=paste("i",j),fitness=data[j,i]))
+            df <- rbind( df, data.frame(Gen=gen,ID=paste("i",j),Fitness=data[j,i]))
         }
     }
     return(df)
 }
 
 #load stuff
-gen1.df <- process.to.df(read.csv("gen1.csv",head=FALSE))
-gen25.df <- process.to.df(read.csv("gen25.csv",head=FALSE))
-gen50.df <- process.to.df(read.csv("gen50.csv",head=FALSE))
-
-
-
-ggplot(gen1.df, aes(fitness, fill = ID)) + geom_bar(position="dodge")
-ggplot(gen25.df, aes(fitness, fill = ID)) + geom_bar(position="dodge")
-ggplot(gen50.df, aes(fitness, fill = ID)) + geom_bar(position="dodge")
+pacman.df <- process.to.df(read.csv("gen1.csv",head=FALSE),1)
+pacman.df <- rbind(pacman.df,process.to.df(read.csv("gen25.csv",head=FALSE),25))
+pacman.df <- rbind(pacman.df,process.to.df(read.csv("gen50.csv",head=FALSE),50))
